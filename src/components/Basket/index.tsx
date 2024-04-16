722e09781c9cbbd6ecd2cac04cc8bfd83124451b
EaGLenok <kapjlconka@gmail.com>
2024-04-16 22:45:05 +0200

    BasketItem
    BasketItem

--- src/components/Basket/index.tsx
+++ src/components/Basket/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import s from "./Basket.module.scss";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import BasketItem from "../BasketItem";
 
-const Basket = () => {
+const Basket: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { itemsBasket } = useAppSelector((state) => state.basketSlice);
+
   return (
     <div className={s.basket_container}>
-      <div className={s.basket_content}>Basket Items Here</div>
-      <div className={s.basket_close}>Close</div>
+      <div className={s.basket_content}>
+        <div className={s.basket_item}>
+          {itemsBasket.map((el: BasketItem, index) => (
+            <BasketItem
+              name={el.name}
+              description={el.description}
+              price={el.price}
+              date_added={el.date_added}
+              imageUrl={el.imageUrl}
+              count={el.count}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={s.basket_close}></div>
     </div>
   );
 };
--- src/components/BasketItem/index.tsx
+++ src/components/BasketItem/index.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import s from "./BasketItem.module.scss";
 
-const BasketItem: React.FC = () => {
-  return <div>index</div>;
+interface BasketItem {
+  name: string;
+  description: string;
+  price: number;
+  date_added: string;
+  imageUrl: string;
+  count: number;
+}
+
+const BasketItem: React.FC<BasketItem> = ({
+  name,
+  description,
+  price,
+  date_added,
+  imageUrl,
+  count,
+}) => {
+  return (
+    <div className={s.basket_items}>
+      <div className={s.basket_item}>
+        <div className={s.image_container}>
+          <img width={130} height={130} src={imageUrl} alt={imageUrl} />
+        </div>
+        <div className={s.item_details}>
+          <p>{name}</p>
+          <p>{price}</p>
+          <p>{count}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BasketItem;
